c787c7f1e42f0618f9a61662a06e8d0bd19c5d5c
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-26 23:29:34 +0500

    Add files via upload

---
+++ Q8.js
@@ -0,0 +1,7 @@
+// You should create four lines that look like this:
+// // console.log(5 + 3)
+// Yzour output should simply be four lines with the number 8 aapering  once on each line .
+console.log(5 + 3);
+console.log(18 - 10);
+console.log(2 * 4);
+console.log(16 / 2);
---
+++ Q8.ts
@@ -0,0 +1,16 @@
+// You should create four lines that look like this:
+// // console.log(5 + 3)
+// Yzour output should simply be four lines with the number 8 aapering  once on each line .
+
+
+// ADDITION
+console.log(5 + 3)
+
+// SUBTRACTION
+console.log(18 - 10)
+
+// MUTIPILICATION
+console.log(2 * 4)
+
+// DIVISION
+console.log(16 / 2)
